b1e4f04243ace702851620686bff87d60276633b
kdhttps <kdhttps@gmail.com>
2020-07-23 17:41:18 +0530

    feat: modularize components, made a new people component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PeopleDetails from './people/PeopleDetails'
 
 function App() {
-
-  const [user, setUser] = useState({
-    firstName: '',
-    lastName: '',
-  })
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  const onChange = (e) => {
-    e.preventDefault();
-    const name = e.target.name;
-    const value = e.target.value;
-    setUser(user => ({ ...user, [name]: value }))
-  };
-
   return (
     <div className="App">
-      <h1>Hello, {user.firstName} {user.lastName}</h1>
-      <form onSubmit={onSubmit}>
-        <input type="text" name="firstName" placeholder="Enter First Name" onChange={onChange} value={user.firstName} /> <br />
-        <input type="text" name="lastName" placeholder="Enter Last Name" onChange={onChange} value={user.lastName} /> <br />
-        <input type={"submit"} value={"Save"} />
-      </form>
+      <h1>Playground</h1>
+      <PeopleDetails />
     </div>
   );
 }
